a6b5d0edda21c4e418b836e20bbe84c9ff8143a8
TheLastJoel <joelbearn@gmail.com>
2022-08-26 11:05:30 +0100

    add advanced learning

---
+++ SwiftUIAdvancedLearning/SwiftUIAdvancedLearning/SwiftUIAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedLearningApp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Joel Bearn on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
